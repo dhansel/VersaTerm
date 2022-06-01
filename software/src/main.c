037b31a2425e3c255fda00853fc3567c535c7edf
David Hansel <david@hansels.net>
2022-05-31 22:15:06 -0400

    Allow selecting configuration at boot using keyboard F1-F10 keys

--- software/src/main.c
+++ software/src/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include "pico/bootrom.h"
 #include "tusb.h"
 #include "framebuf.h"
 #include "serial.h"
@@ -11,6 +12,14 @@
 #include "font.h"
 #include "pins.h"
 #include "sound.h"
+
+
+// see comment at start of main()
+#define BOOTSEL_TIMEOUT_MS 1500
+static absolute_time_t bootsel_timeout = 0;
+static const uint32_t bootsel_magic[] = {0xf01681de, 0xbd729b29, 0xd359be7a};
+static uint32_t __uninitialized_ram(bootsel_magic_ram)[count_of(bootsel_magic)];
+static uint16_t ignore_key = HID_KEY_NONE;
 
 
 void apply_settings()
@@ -33,40 +42,52 @@ void run_tasks(bool processInput)
   
   // process serial input
   serial_task(processInput);
+
+  // handle bootsel mechanism timeout
+  if( bootsel_timeout>0 && get_absolute_time()>=bootsel_timeout )
+    {
+      bootsel_timeout = 0;
+      for(uint i=0; i<count_of(bootsel_magic); i++) 
+        bootsel_magic_ram[i] = 0;
+    }
   
   // process keyboard input
   keyboard_task();
   if( processInput && keyboard_num_keypress()>0 )
     {
       uint16_t key = keyboard_read_keypress();
-      
-      if( key==HID_KEY_F12 )
+      if( key!=ignore_key )
         {
-          if( config_menu() ) apply_settings();
-        }
-      else if( keyboard_ctrl_pressed(key) && (key&0xFF)==HID_KEY_F12 )
-        {
-          if( config_load(0xFF) ) apply_settings();
-        }
-      else if( key==HID_KEY_F11 )
-        keyboard_macro_record_startstop();
-      else if( keyboard_ctrl_pressed(key) && (key&0xFF)>=HID_KEY_F1 && (key&0xFF)<=HID_KEY_F10 )
-        {
-          uint8_t vol = config_get_audible_bell_volume();
-          if( config_load((key&0xFF)-HID_KEY_F1) )
+          ignore_key = HID_KEY_NONE;
+
+          if( key==HID_KEY_F12 )
             {
-              apply_settings();
-              sound_play_tone(880, 50, vol, false);
+              if( config_menu() ) apply_settings();
+            }
+          else if( keyboard_ctrl_pressed(key) && (key&0xFF)==HID_KEY_F12 )
+            {
+              if( config_load(0xFF) ) apply_settings();
+            }
+          else if( key==HID_KEY_F11 )
+            keyboard_macro_record_startstop();
+          else if( keyboard_ctrl_pressed(key) && (key&0xFF)>=HID_KEY_F1 && (key&0xFF)<=HID_KEY_F10 )
+            {
+              uint8_t vol = config_get_audible_bell_volume();
+              if( config_load((key&0xFF)-HID_KEY_F1) )
+                {
+                  apply_settings();
+                  sound_play_tone(880, 50, vol, false);
+                }
+              else
+                {
+                  sound_play_tone(880, 50, vol, true); wait(50);
+                  sound_play_tone(880, 50, vol, true); wait(50);
+                  sound_play_tone(880, 50, vol, false); 
+                }
             }
           else
-            {
-              sound_play_tone(880, 50, vol, true); wait(50);
-              sound_play_tone(880, 50, vol, true); wait(50);
-              sound_play_tone(880, 50, vol, false); 
-            }
+            terminal_process_key(key);
         }
-      else
-        terminal_process_key(key);
     }
 }
 
@@ -80,6 +101,30 @@ void wait(uint32_t milliseconds)
 
 int main()
 {
+  // The following mechanism is fundamentally the same as the pico_bootsel_via_double_reset
+  // library but that library uses a busy wait until the maximum time for the double-tap
+  // has expired. Implementing it ourselves here instead allows to use that wait time
+  // for initialization.
+  uint i;
+  for(i=0; i<count_of(bootsel_magic) && bootsel_magic_ram[i]==bootsel_magic[i]; i++);
+
+  if( i<count_of(bootsel_magic) )
+    {
+      // arm mechanism and set timeout
+      for(i=0; i<count_of(bootsel_magic); i++) 
+        bootsel_magic_ram[i] = bootsel_magic[i];
+      bootsel_timeout = make_timeout_time_ms(BOOTSEL_TIMEOUT_MS);
+    }
+  else
+    {
+      // disarm our mechanism so pressing RESET in boot loader starts up normally
+      for(i=0; i<count_of(bootsel_magic); i++) 
+        bootsel_magic_ram[i] = 0;
+      
+      // boot into boot-loader using GPIO25 (on-board LED) as activity LED
+      reset_usb_boot(1<<25, 0);
+    }
+  
   config_init();
   stdio_uart_init_full(PIN_UART_ID, 300, PIN_UART_TX, PIN_UART_RX);
   serial_init();
@@ -104,16 +149,32 @@ int main()
   // initialize keyboard
   keyboard_init();
 
+  // allow some time for keyboard(s) to initialize
+  wait(tuh_inited() ? 1500 : 250);
+  
   // if DEFAULTS button and CTRL key is pressed then force DVI
   if( !gpio_get(PIN_DEFAULTS) )
-    {
-      // give some time for keyboard to fully initialize
-      wait(1500);
-      framebuf_init((keyboard_get_current_modifiers() & (KEYBOARD_MODIFIER_LEFTCTRL|KEYBOARD_MODIFIER_RIGHTCTRL))!=0);
-    }
+    framebuf_init((keyboard_get_current_modifiers() & (KEYBOARD_MODIFIER_LEFTCTRL|KEYBOARD_MODIFIER_RIGHTCTRL))!=0);
   else
-    framebuf_init(false);
+    {
+      // check F1-F10 keys for startup config
+      while( keyboard_num_keypress()>0 )
+        {
+          uint16_t c = keyboard_read_keypress();
+          if( (c & 0xFF)>=HID_KEY_F1 && (c & 0xFF)<=HID_KEY_F10 && config_load((c & 0xFF)-HID_KEY_F1) )
+            {
+              // apply settings (may have changed)
+              keyboard_apply_settings();
+              serial_apply_settings();
+              // ignore further repeats of Fx key 
+              ignore_key = c;
+              break;
+            }
+        }
 
+      framebuf_init(false);
+    }
+  
   terminal_init();
   sound_init();
   config_show_splash();
